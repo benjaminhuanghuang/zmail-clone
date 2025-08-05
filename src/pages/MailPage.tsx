dcfca07c55a24e1205bfb2f24a1d1754989c44c4
benjanmin huang <benjaminhuanghuang@gmail.com>
2025-08-04 22:17:00 -0700

    mail list, tab

--- src/pages/MailPage.tsx
+++ src/pages/MailPage.tsx
@@ -1,16 +1,41 @@
 import MailPageLayout from "@/components/MailPageLayout";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ChevronDown } from "lucide-react";
 import NoMails from "@/components/NoMails";
+import { Checkbox } from "@/components/ui/checkbox";
 
 const MailPage = () => {
   return (
     <MailPageLayout>
       <Tabs defaultValue="primary">
-        <TabsList>
-          <TabsTrigger value="primary">Primary</TabsTrigger>
-          <TabsTrigger value="social">Social</TabsTrigger>
-          <TabsTrigger value="personal">Personal</TabsTrigger>
-        </TabsList>
+        <div className="flex items-center mb-2">
+          {/* Compact Dropdown */}
+          <Checkbox />
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <ChevronDown className="h-3 w-3 m-2" />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="start">
+              <DropdownMenuItem onClick={() => {}}>All mail</DropdownMenuItem>
+              <DropdownMenuItem onClick={() => {}}>None</DropdownMenuItem>
+              <DropdownMenuItem onClick={() => {}}>Read</DropdownMenuItem>
+              <DropdownMenuItem onClick={() => {}}>Unread</DropdownMenuItem>
+              <DropdownMenuItem onClick={() => {}}>Starred</DropdownMenuItem>
+              <DropdownMenuItem onClick={() => {}}>Unstarred</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <TabsList>
+            <TabsTrigger value="primary">Primary</TabsTrigger>
+            <TabsTrigger value="social">Social</TabsTrigger>
+            <TabsTrigger value="personal">Personal</TabsTrigger>
+          </TabsList>
+        </div>
         <TabsContent value="primary">
           <NoMails />
         </TabsContent>
