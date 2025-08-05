f7fa4def2b610d10e0927a4a1386d03e94aa0931
benjanmin huang <benjaminhuanghuang@gmail.com>
2025-08-04 19:53:21 -0700

    nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,13 @@
 import { Button } from "./ui/button";
-import { Settings } from "lucide-react";
+import { Settings, User } from "lucide-react";
 
 import { SidebarTrigger } from "./ui/sidebar";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Navbar = () => {
   return (
@@ -11,6 +17,24 @@ const Navbar = () => {
 
       {/* RIGHT */}
       <div className="flex items-center gap-4">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" size="icon">
+              <User className="h-5 w-5" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuCheckboxItem checked>
+              List view
+            </DropdownMenuCheckboxItem>
+            <DropdownMenuCheckboxItem>
+              Vertical split view
+            </DropdownMenuCheckboxItem>
+            <DropdownMenuCheckboxItem>
+              Horizontal split view
+            </DropdownMenuCheckboxItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
         <Button variant="ghost" size="icon" className="size-8">
           <Settings />
         </Button>
